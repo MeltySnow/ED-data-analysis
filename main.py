5762e234d25947125145af0d9b6546491075b65f
Owen Mahnke <owen.mahnke@googlemail.com>
2023-06-20 13:54:48 +0100

    Add arguments to argparse which are to be implemented later

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ import argparse
 #Configure argparse for handling command line arguments
 parser: argparse.ArgumentParser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("experimentIDs", action="store", help="List of experiment IDs to include", nargs='*')
+parser.add_argument("-o", "--output", action="store", help="Specify name out output file. Default is out.html")
+parser.add_argument("-d", "--dashboard", action="store", help="Specify the ID of the Notion dashboard to read from")
+parser.add_argument("-x", "--exclude", action="store_true", help="Processes all experiments marked as \"Completed\", excluding those supplied as positional argiments")
 
 config: argparse.Namespace = parser.parse_args()
 
 try:
-	edAnalysis = EDAnalysisManager(config.experimentIDs)
+	edAnalysis = EDAnalysisManager(config)
 except Exception as e:
 	print (e, file=sys.stderr)
 	sys.exit(1)
